1ff9a9da91e1d82f316c1e0feb790fbdb8fe1df7
Maxim Medyanik <tvoygluk@gmail.com>
2019-06-16 00:03:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,15 @@ try
 
   openModalWindowFindUs.addEventListener("click", function (evt)
   {
-    evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+    if (evt.preventDefault)
+    {
+      evt.preventDefault();
+    }
+    else
+    {
+      evt.returnValue = false;
+    }
+
     modalWindowFindUs.classList.add("make-modal-visible");
 
     if (storageFindUsName && storageFindUsEmale)
@@ -42,7 +50,16 @@ try
   {
     if (!modalWindowFindUsNameField.value || !modalWindowFindUsEmaleField.value || !modalWindowFindUsLetterField.value)
     {
-      evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+
+      if (evt.preventDefault)
+      {
+        evt.preventDefault();
+      }
+      else
+      {
+        evt.returnValue = false;
+      }
+
       modalWindowFindUs.classList.remove("modal-error");
       modalWindowFindUs.offsetWidth = modalWindowFindUs.offsetWidth;
       modalWindowFindUs.classList.add("modal-error");
@@ -59,7 +76,15 @@ try
 
   closeModalWindowFindUs.addEventListener("click", function (evt)
   {
-    evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+    if (evt.preventDefault)
+    {
+      evt.preventDefault();
+    }
+    else
+    {
+      evt.returnValue = false;
+    }
+
     modalWindowFindUs.classList.remove("make-modal-visible");
     modalWindowFindUs.classList.remove("modal-error");
   });
@@ -70,7 +95,15 @@ try
     {
       if (modalWindowFindUs.classList.contains("make-modal-visible"))
       {
-        evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+        if (evt.preventDefault)
+        {
+          evt.preventDefault();
+        }
+        else
+        {
+          evt.returnValue = false;
+        }
+
         modalWindowFindUs.classList.remove("make-modal-visible");
         modalWindowFindUs.classList.remove("modal-error");
       }
@@ -83,13 +116,29 @@ try
 
   openModalWindowMap.addEventListener("click", function (evt)
   {
-    evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+    if (evt.preventDefault)
+    {
+      evt.preventDefault();
+    }
+    else
+    {
+      evt.returnValue = false;
+    }
+
     modalWindowMap.classList.add("make-modal-visible");
   });
 
   closeModalWindowMap.addEventListener("click", function (evt)
   {
-    evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+    if (evt.preventDefault)
+    {
+      evt.preventDefault();
+    }
+    else
+    {
+      evt.returnValue = false;
+    }
+
     modalWindowMap.classList.remove("make-modal-visible");
   });
 
@@ -99,7 +148,15 @@ try
     {
       if (modalWindowMap.classList.contains("make-modal-visible"))
       {
-        evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+        if (evt.preventDefault)
+        {
+          evt.preventDefault();
+        }
+        else
+        {
+          evt.returnValue = false;
+        }
+
         modalWindowMap.classList.remove("make-modal-visible");
       }
     }
@@ -120,20 +177,44 @@ for (index = 0; index < openModalWindowCartAdded.length; index++)
   button = openModalWindowCartAdded[index];
   button.addEventListener('click', function (evt)
   {
-      evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+      if (evt.preventDefault)
+      {
+        evt.preventDefault();
+      }
+      else
+      {
+        evt.returnValue = false;
+      }
+
       modalWindowCartAdded.classList.add("make-modal-visible-flex");
   });
 }
 
 closeModalWindowCartAdded.addEventListener("click", function (evt)
 {
-  evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+  if (evt.preventDefault)
+  {
+    evt.preventDefault();
+  }
+  else
+  {
+    evt.returnValue = false;
+  }
+
   modalWindowCartAdded.classList.remove("make-modal-visible-flex");
 });
 
 anotherCloseModalWindowCartAdded.addEventListener("click", function (evt)
 {
-  evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+  if (evt.preventDefault)
+  {
+    evt.preventDefault();
+  }
+  else
+  {
+    evt.returnValue = false;
+  }
+
   modalWindowCartAdded.classList.remove("make-modal-visible-flex");
 });
 
@@ -143,7 +224,15 @@ window.addEventListener("keydown", function(evt)
   {
     if (modalWindowCartAdded.classList.contains("make-modal-visible-flex"))
     {
-      evt.preventDefault ? evt.preventDefault() : (evt.returnValue = false);
+      if (evt.preventDefault)
+      {
+        evt.preventDefault();
+      }
+      else
+      {
+        evt.returnValue = false;
+      }
+
       modalWindowCartAdded.classList.remove("make-modal-visible-flex");
     }
   }
