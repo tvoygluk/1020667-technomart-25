ad0e4ad543c8948569c9698546ed2213ebcfb974
Maxim Medyanik <tvoygluk@gmail.com>
2019-06-15 14:30:11 +0300

    Настроил скрипт для модальных окон

---
+++ js/script.js
@@ -0,0 +1,150 @@
+try
+{
+  var openModalWindowFindUs = document.querySelector(".find-us-btn");
+  var modalWindowFindUs = document.querySelector(".modal-find-us");
+  var closeModalWindowFindUs = modalWindowFindUs.querySelector(".modal-close-find-us");
+  var modalWindowFindUsForm = modalWindowFindUs.querySelector(".find-us-form");
+  var modalWindowFindUsNameField = modalWindowFindUs.querySelector("[name=find-us-name]");
+  var modalWindowFindUsEmaleField = modalWindowFindUs.querySelector("[name=find-us-male]");
+  var modalWindowFindUsLetterField = modalWindowFindUs.querySelector("[name=find-us-letter]");
+  var isStorageSupport = true;
+  var storageFindUsName = "";
+  var storageFindUsEmale = "";
+
+  try
+  {
+    storageFindUsName = localStorage.getItem("modalWindowFindUsNameField");
+    storageFindUsEmale = localStorage.getItem("modalWindowFindUsEmaleField");
+  }
+  catch (err)
+  {
+    isStorageSupport = false;
+  }
+
+  openModalWindowFindUs.addEventListener("click", function (evt)
+  {
+    evt.preventDefault();
+    modalWindowFindUs.classList.add("make-modal-visible");
+
+    if (storageFindUsName && storageFindUsEmale)
+    {
+      modalWindowFindUsNameField.value = storageFindUsName;
+      modalWindowFindUsEmaleField.value = storageFindUsEmale;
+      modalWindowFindUsLetterField.focus();
+    }
+    else
+    {
+      modalWindowFindUsNameField.focus();
+    }
+  });
+
+  modalWindowFindUsForm.addEventListener("submit", function (evt)
+  {
+    if (!modalWindowFindUsNameField.value || !modalWindowFindUsEmaleField.value || !modalWindowFindUsLetterField.value)
+    {
+      evt.preventDefault();
+      modalWindowFindUs.classList.remove("modal-error");
+      modalWindowFindUs.offsetWidth = modalWindowFindUs.offsetWidth;
+      modalWindowFindUs.classList.add("modal-error");
+    }
+    else
+    {
+      if (isStorageSupport)
+      {
+        localStorage.setItem("modalWindowFindUsNameField", modalWindowFindUsNameField.value);
+        localStorage.setItem("modalWindowFindUsEmaleField", modalWindowFindUsEmaleField.value);
+      }
+    }
+  });
+
+  closeModalWindowFindUs.addEventListener("click", function (evt)
+  {
+    evt.preventDefault();
+    modalWindowFindUs.classList.remove("make-modal-visible");
+    modalWindowFindUs.classList.remove("modal-error");
+  });
+
+  window.addEventListener("keydown", function(evt)
+  {
+    if (evt.keyCode === 27)
+    {
+      if (modalWindowFindUs.classList.contains("make-modal-visible"))
+      {
+        evt.preventDefault();
+        modalWindowFindUs.classList.remove("make-modal-visible");
+        modalWindowFindUs.classList.remove("modal-error");
+      }
+    }
+  });
+
+  var openModalWindowMap = document.querySelector(".map-google-link");
+  var modalWindowMap = document.querySelector(".modal-map");
+  var closeModalWindowMap = modalWindowMap.querySelector(".modal-close-map");
+
+  openModalWindowMap.addEventListener("click", function (evt)
+  {
+    evt.preventDefault();
+    modalWindowMap.classList.add("make-modal-visible");
+  });
+
+  closeModalWindowMap.addEventListener("click", function (evt)
+  {
+    evt.preventDefault();
+    modalWindowMap.classList.remove("make-modal-visible");
+  });
+
+  window.addEventListener("keydown", function(evt)
+  {
+    if (evt.keyCode === 27)
+    {
+      if (modalWindowMap.classList.contains("make-modal-visible"))
+      {
+        evt.preventDefault();
+        modalWindowMap.classList.remove("make-modal-visible");
+      }
+    }
+  });
+}
+catch (err)
+{
+
+}
+
+var modalWindowCartAdded = document.querySelector(".modal-cart-added");
+var closeModalWindowCartAdded = modalWindowCartAdded.querySelector(".modal-close-cart-added");
+var anotherCloseModalWindowCartAdded = modalWindowCartAdded.querySelector(".continue-shopping");
+var openModalWindowCartAdded = document.querySelectorAll(".btn-buy"), index, button;
+
+for (index = 0; index < openModalWindowCartAdded.length; index++)
+{
+  button = openModalWindowCartAdded[index];
+  button.addEventListener('click', function (evt)
+  {
+      evt.preventDefault();
+      modalWindowCartAdded.classList.add("make-modal-visible-flex");
+  });
+}
+
+closeModalWindowCartAdded.addEventListener("click", function (evt)
+{
+  evt.preventDefault();
+  modalWindowCartAdded.classList.remove("make-modal-visible-flex");
+});
+
+anotherCloseModalWindowCartAdded.addEventListener("click", function (evt)
+{
+  evt.preventDefault();
+  modalWindowCartAdded.classList.remove("make-modal-visible-flex");
+});
+
+window.addEventListener("keydown", function(evt)
+{
+  if (evt.keyCode === 27)
+  {
+    if (modalWindowCartAdded.classList.contains("make-modal-visible-flex"))
+    {
+      evt.preventDefault();
+      modalWindowCartAdded.classList.remove("make-modal-visible-flex");
+    }
+  }
+});
